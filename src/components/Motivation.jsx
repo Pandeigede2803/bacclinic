d05311a49371eea6400ee81ce65b78e78825f4b1
Pandeigede2803 <131873435+Pandeigede2803@users.noreply.github.com>
2024-01-26 14:56:56 +0800

    tes testimoni

--- src/components/Motivation.jsx
+++ src/components/Motivation.jsx
@@ -5,7 +5,7 @@ const Motivation = () => {
     <div>
       <div className="bg-[url('https://ik.imagekit.io/m1akscp5q/imagebackgroundkarina.png?updatedAt=1706168308850')] md:h-[909px] md:w-full  w-full h-[257px] bg-cover bg-center  bg-opacity-50 flex text-center align-middle">
         <p className=" w-[163px] md:w-[545px] md:h-[348px] text-left md:text-[49px] text-[15px] my-auto mx-6 md:ml-24 font-primary">
-        "Beauty is a journey, not a destination. We are proud to be a part of your journey."
+        &quot;Beauty is a journey, not a destination. We are proud to be a part of your journey.&quot;
         </p>
       </div>
     </div>
