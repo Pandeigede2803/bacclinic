23a672800cd9de4c782dac2312ce7fdb505c8b4e
Pandeigede2803 <131873435+Pandeigede2803@users.noreply.github.com>
2024-01-30 12:57:51 +0800

    Update

--- src/components/ctaSection.jsx
+++ src/components/ctaSection.jsx
@@ -5,12 +5,12 @@ const ctaSection = () => {
   return (
     <section>
         <Image
-        src=""
-        width={}
-        height={}/>
+        src="https://ik.imagekit.io/m1akscp5q/CTA%20section%20(1).png?updatedAt=1706590565877"
+        width={2880}
+        height={1068}/>
       <h1>cta section</h1>
     </section>
   )
 }
 
-export default ctaSection;
+export default ctaSection;;
