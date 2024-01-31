ca8f32de8625bdce53f6368fb17f0e3ae3aaa4fb
Pandeigede2803 <131873435+Pandeigede2803@users.noreply.github.com>
2024-01-31 16:03:11 +0800

    productcards edit

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import ProductCard from './ProductCard';
-import ProductData from './ProductData'; // Ensure this is an array of objects
+import React from "react";
+import ProductCard from "./ProductCard";
+import ProductData from "./ProductData"; // Ensure this is an array of objects
 
 const ProductGrid = () => {
   return (
-
-    <div className=' font-primary'>
-        <h1 className=' text-center mt-10  text-2xl'>SKINCARE PRODUCT</h1>
-    <div className='my-6 w-full flex flex-col overflow-x-auto  md:flex-row '>
- 
-    <div className="flex flex-nowrap gap-2 mx-auto w-fit"> {/* Adjust grid layout as needed */}
-    {ProductData.map((product) => (
-          <div className="min-w-[20%] sm:min-w-[30%] md:min-w-[23%] lg:min-w-[18%]"> {/* Adjust the width per breakpoint */}
-            <ProductCard
-      
-              id={product.id}
-              src={product.src}
-              title={product.title}
-              alt={product.alt}
-              link={product.link}
-            />
-          </div>
-        ))}
-    </div>
-    </div>
+    <div className=" font-primary">
+      <h1 className=" text-center mt-10  text-2xl">SKINCARE PRODUCT</h1>
+      <div className="my-6 w-full flex flex-col overflow-x-auto  md:flex-row ">
+        <div className="flex flex-nowrap gap-2 mx-auto w-fit">
+          {" "}
+          {/* Adjust grid layout as needed */}
+          {ProductData.map((product) => (
+            <div className="min-w-[20%] sm:min-w-[30%] md:min-w-[23%] lg:min-w-[18%]">
+              {" "}
+              {/* Adjust the width per breakpoint */}
+              <ProductCard
+                id={product.id}
+                src={product.src}
+                title={product.title}
+                alt={product.alt}
+                link={product.link}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default ProductGrid;;
+export default ProductGrid;
