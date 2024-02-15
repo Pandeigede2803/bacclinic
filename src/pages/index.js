7a9a080affad4844a1ecb66f23bae6a539f5f3f4
Pandeigede2803 <131873435+Pandeigede2803@users.noreply.github.com>
2024-02-15 16:59:07 +0800

    SEO UPDATE

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { FloatingWhatsApp } from 'react-floating-whatsapp'
-
+import React from 'react';
+import Head from 'next/head';
+import { FloatingWhatsApp } from 'react-floating-whatsapp';
 import Image from "next/image";
 import CustomLinkComponent from "@/components/CustomLinkComponent";
 import Header from "@/components/Header";
@@ -17,16 +17,29 @@ import Form from '@/components/Form';
 import Article from '@/components/Article';
 
 export default function Home() {
-
   return (
-    <main>
-      <div className="">
-        <Header />
-      </div>
-      <div className="mt-[909px]">
-        <SliderCards />
-      </div>
-      <div>
+    <>
+      <Head>
+        <title>Beauty Clinic Denpasar Bali - BAC Clinic | Perawatan Estetika Terdepan</title>
+        <meta name="description" content="BAC Clinic adalah pilihan terbaik untuk perawatan estetika di Denpasar, Bali. Dengan teknologi terkini, kami menawarkan layanan Botox, filler, terapi kulit, dan banyak lagi untuk membantu Anda tampil percaya diri." />
+        <meta property="og:title" content="Beauty Clinic Denpasar Bali - BAC Clinic | Perawatan Estetika Terdepan" />
+        <meta name="keywords" content="Beauty Clinic Denpasar,Klinik kecantikan terdekat, Klinik kecantikan denpasar, Klinik kecantikan Bali, Nearest Beauty Clinic, BAC Clinic Bali, Perawatan Estetika,Aesthetic Treatments, Skin Treatments, Dermatology, Aesthetic Clinic,  Botox, Filler, Terapi Kulit, Klinik Kecantikan di Bali" />
+        <meta property="og:description" content="BAC Clinic adalah pilihan terbaik untuk perawatan estetika di Denpasar, Bali. Dengan teknologi terkini, kami menawarkan layanan Botox, filler, terapi kulit, dan banyak lagi untuk membantu Anda tampil percaya diri." />
+        <meta property="og:url" content="https://bacclinic.id" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://ik.imagekit.io/m1akscp5q/background%20image.png?updatedAt=1707296969772" />
+        <link rel="canonical" href="https://bacclinic.id" />
+      </Head>
+      <main>
+        <div>
+          <Header />
+        </div>
+        {/* Lanjutkan dengan komponen lainnya */}
+        <div className="mt-[909px]">
+          <SliderCards />
+        </div>
+
+        <div>
         <PromoSection />
       </div>
       <div>
@@ -49,14 +62,15 @@ export default function Home() {
         <Article/>
 
       </div>
-  
-      <FloatingWhatsApp
-        phoneNumber="6287777939598"
-        accountName="BAC CLINIC"
-        className="whatsapp-icon"
-        avatar="https://ik.imagekit.io/m1akscp5q/logo%20bac%20vertikal.png?updatedAt=1706761240275"
-        chatMessage="Hello there! 🤝 How can we help?"
-      />
-    </main>
+        {/* Sisipkan semua komponen lain seperti yang telah kamu definisikan */}
+        <FloatingWhatsApp
+          phoneNumber="6287777939598"
+          accountName="BAC CLINIC"
+          className="whatsapp-icon"
+          avatar="https://ik.imagekit.io/m1akscp5q/logo%20bac%20vertikal.png?updatedAt=1706761240275"
+          chatMessage="Hello there! 🤝 How can we help?"
+        />
+      </main>
+    </>
   );
 }
